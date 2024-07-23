c38be40a16cd63b52ad5f6ad165c8482b99e109a
thinkAmi <dev.thinkami@gmail.com>
2024-07-23 22:11:28 +0900

    change railroads icon

---
+++ src/main/kotlin/com/github/thinkami/railroads/icons/RailroadsIcons.kt
@@ -0,0 +1,8 @@
+package com.github.thinkami.railroads.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object RailroadsIcons {
+    @JvmField
+    val RailroadsToolWindow = IconLoader.getIcon("/icons/railroads_toolwindow.svg", javaClass)
+}
