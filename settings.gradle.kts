eb37d8c44defa68ace4e595eb1532c26d1d31ad1
thinkAmi <1299734+thinkAmi@users.noreply.github.com>
2026-05-08 14:32:42 +0900

    chore: migrate Gradle and CI configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,35 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+        id("org.jetbrains.changelog") version "2.2.1"
+        id("org.jetbrains.intellij.platform") version "2.8.0"
+        id("org.jetbrains.intellij.platform.settings") version "2.8.0"
+        id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    }
+}
+
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention")
+    id("org.jetbrains.intellij.platform.settings")
 }
 
 rootProject.name = "railroads"
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+
+        intellijPlatform {
+            defaultRepositories()
+        }
+    }
+}
